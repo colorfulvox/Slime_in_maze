fc43abf764d6dbf197556c1a04975a2938876e9b
chu-huiwong <eliteit@naver.com>
2021-04-07 16:48:03 +0900

    접수실 [튜툐,일반,퀘스트접수 이동] 구현 , 코드 추가 공지,대기실 이동 미구현

---
+++ Slime in maze (update)/1. 화면 구성/1_2. 메뉴/1_2_1. 접수실/코드/Reception room/Assets/code/graphsize.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class graphsize : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(1280, 720, false);
+    }
+}
